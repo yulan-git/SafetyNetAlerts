db77208fcb39a93ce3f459d1b2813c1a1ae116a8
Lafferrière <flora.lafferriere@gmail.com>
2022-03-31 17:20:39 +0200

    first commit

---
+++ safetynetalerts/src/main/java/com/flora/safetynetalerts/SafetynetalertsApplication.java
@@ -0,0 +1,13 @@
+package com.flora.safetynetalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetalertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetalertsApplication.class, args);
+	}
+
+}
